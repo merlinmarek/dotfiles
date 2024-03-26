948231740ccd4b2413e725a9c65eccd99debd602
Merlin Marek <merlin.marek@impact-it-services.de>
2024-03-26 15:52:28 +0100

    refactor(nvim): fix comment, reorder functions

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,6 +2,7 @@ require("user.options")
 require("user.keymaps")
 require("user.lazy")
 
+-- apply organize imports no save
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.go",
   callback = function()
@@ -11,7 +12,6 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
     for cid, res in pairs(result or {}) do
       for _, r in pairs(res.result or {}) do
-        -- print(vim.inspect(r))
         if r.edit then
           local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
           vim.lsp.util.apply_workspace_edit(r.edit, enc)
@@ -21,26 +21,6 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     vim.lsp.buf.format({ async = false })
   end,
 })
-
---vim.api.nvim_create_autocmd("BufWritePre", {
---  pattern = "*.py",
---  callback = function()
---    local bufnr = vim.api.nvim_get_current_buf()
---    local params = vim.lsp.util.make_range_params()
---    params.context = {
---      triggerKind = vim.lsp.protocol.CodeActionTriggerKind.Invoked,
---      diagnostics = vim.lsp.diagnostic.get_line_diagnostics(),
---    }
---
---    local actions = vim.lsp.buf_request_sync(bufnr, "textDocument/codeAction", params)
---    for _, action in ipairs(actions) do
---      print(vim.inspect(action), vim.inspect(getmetatable(action)), action.edit, action.command)
---    end
---  end
---})
-
--- do not yank when pasting over visual selection
-vim.cmd("xnoremap <expr> p '\"_d\"'.v:register.'P'")
 
 -- return to the last edited line when reopening a file
 vim.api.nvim_create_autocmd("BufRead", {
@@ -62,6 +42,9 @@ vim.api.nvim_create_autocmd("BufRead", {
     })
   end,
 })
+
+-- do not yank when pasting over visual selection
+vim.cmd("xnoremap <expr> p '\"_d\"'.v:register.'P'")
 
 -- open terminal with <leader>t
 vim.cmd("nnoremap <leader>t :let $VIM_DIR=expand('%:p:h')<cr>:split \\| terminal<cr>cd $VIM_DIR<cr>c<cr>")
@@ -94,11 +77,9 @@ vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 -- abbreviations
 vim.cmd("iabbrev rud refactor: update dependencies")
 
-require("local")
-
 vim.cmd.colorscheme("16term")
 
--- reload colorscheme when it changes watch colorscheme
+-- reload colorscheme when it changes
 local function watch_colorscheme()
   local path = vim.fn.stdpath("config") .. "/colors/16term.lua"
   local event = vim.loop.new_fs_event()
@@ -117,3 +98,5 @@ end
 watch_colorscheme()
 
 vim.cmd("set spelllang=de")
+
+require("local")
